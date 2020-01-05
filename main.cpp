0d02291442dc9c446cd46c9afc52aefb5aa0d842
Oleksandr Abasov <axkb.od@gmail.com>
2020-01-05 15:00:36 +0200

    Ch2 Draw game borders

--- main.cpp
+++ main.cpp
@@ -2,15 +2,19 @@
 
 using namespace std;
 
+const int MAX_COLUMNS = 20;
+const int MAX_ROWS = 10;
+
 // forward declaration - предварительное объявление
-void draw();
+void drawGame();
+void drawBox();
 
 int main() {
     // Переход в curses-режим
     initscr();
 
     // Отрисовать содержимое
-    draw();
+    drawGame();
 
     // Ожидание нажатия какой-либо клавиши пользователем
     getch();
@@ -21,15 +25,32 @@ int main() {
     return 0;
 }
 
-void draw() {
+void drawGame() {
     // Очистить экран
     clear();
 
-    // Запись текста в буфер
+    // Draw game borders
+    drawBox();
+
+    // Запись текста в буфер (y, x)
     mvprintw(5, 5, "Hello World");
 
     // Вывод данных из буфера на экран
     refresh();
+}
+
+void drawBox() {
+    // draw celling and floor
+    for (int j = 0; j < MAX_COLUMNS; j++) {
+        mvprintw(0, j, "_");
+        mvprintw(MAX_ROWS, j, "-");
+    }
+
+    // draw left and right walls
+    for (int i = 1; i < MAX_ROWS; i++) {
+        mvprintw(i, 0, "|");
+        mvprintw(i, MAX_COLUMNS - 1, "|");
+    }
 }
 
 
